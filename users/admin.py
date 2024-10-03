ec6a2cadeb82badbc494e26f0f8ff7d0d5b7fdf9
safyro <vektorn1212@gmail.com>
2024-10-03 13:07:13 +0300

    register user model in django admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """
+    User model for django admin.
+
+    It is possible to search by email and last_name.
+    """
+
+    list_display = (
+        "email",
+        "first_name",
+        "last_name",
+    )
+    search_fields = (
+        "email",
+        "last_name",
+    )
