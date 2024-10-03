9bb7f7369864362889533edaaeaea80c6b1b48f7
safyro <vektorn1212@gmail.com>
2024-10-03 13:17:43 +0300

    added user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,30 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class User(AbstractUser):
+    """Custom user model with email field."""
+
+    username = None
+
+    email = models.EmailField(
+        max_length=64,
+        unique=True,
+        verbose_name=_("Email Address"),
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        """
+        String representation of User model.
+
+        Uses the model first_name, last_name and email fields.
+        """
+        return f"{self.first_name} {self.last_name} - {self.email}"
+
+    class Meta:
+        verbose_name = _("User")
+        verbose_name_plural = _("Users")
